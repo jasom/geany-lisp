47c45b98f519520b66283950f22b6aa5bdd6ccc3
Jason Miller <aidenn0@geocities.com>
2016-06-08 00:51:56 -0700

    Add CFLAGS to config script

--- config.sh
+++ config.sh
@@ -2,4 +2,5 @@ PREFIX="${PREFIX:-/usr}"
 cat > tup.config <<EOF
 CONFIG_INSTALL_DIR=$PREFIX/share/glisp
 CONFIG_GEANY_PLUGIN_DIR=${GEANY_PLUGIN_DIR:-$HOME/.config/geany/plugins}
+CONFIG_CFLAGS=${CFLAGS:--g3}
 EOF
