7f61d0f58539cc3b3dee2eb85e0641b80f808c14
Jason Miller <aidenn0@geocities.com>
2016-07-25 20:06:42 -0700

    Label stderr/stdout with name of subtest

--- test/run-tests.sh
+++ test/run-tests.sh
@@ -3,6 +3,8 @@ cd utility-tests
 rm -f test.stdout test.stderr
 for item in *.sh; do
     printf '%s ... ' "$(basename "$item")" >&2
+    printf '%s:\n' "$item" >>test.stdout
+    printf '%s:\n' "$item" >>test.stderr
     if sh "$item" >>test.stdout 2>>test.stderr; then
         printf 'Passed\n'
     else
@@ -10,4 +12,8 @@ for item in *.sh; do
         sh 99-cleanup.sh
         exit 1
     fi
+    echo >> test.stdout
+    echo >> test.stderr
 done
+echo >> test.stdout
+echo >> test.stderr
