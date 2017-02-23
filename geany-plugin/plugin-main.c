0419dcc9ebedb9990c1848679c5b9b91a3e4b196
Jason Miller <aidenn0@geocities.com>
2017-02-22 17:40:38 -0800

    Update to newer geany ABI

--- geany-plugin/plugin-main.c
+++ geany-plugin/plugin-main.c
@@ -2,20 +2,20 @@
 
 GeanyPlugin     *geany_plugin;
 GeanyData       *geany_data;
-GeanyFunctions  *geany_functions;
+//GeanyFunctions  *geany_functions;
 
-PLUGIN_VERSION_CHECK(211)
-PLUGIN_SET_INFO("Lisp Development", "Lisp Development",
-                "1.0", "John Doe <john.doe@example.org>");
-PLUGIN_KEY_GROUP(lisp, 2);
+//PLUGIN_VERSION_CHECK(211)
+//PLUGIN_KEY_GROUP(lisp, 2);
 //static GtkWidget *main_menu_item = NULL;
 
 static gboolean notify_cb(GObject *obj, GeanyEditor *ed, SCNotification *nt, gpointer user_data);
 static void project_open_cb(G_GNUC_UNUSED GObject * obj, G_GNUC_UNUSED GKeyFile * config, G_GNUC_UNUSED gpointer user_data);
 static void project_close_cb(G_GNUC_UNUSED GObject * obj, G_GNUC_UNUSED GKeyFile * config, G_GNUC_UNUSED gpointer user_data);
 static void project_save_cb(G_GNUC_UNUSED GObject * obj, GKeyFile * config, G_GNUC_UNUSED gpointer user_data);
+static gboolean myplugin_init(GeanyPlugin *plugin, gpointer pdata);
+static void myplugin_cleanup(GeanyPlugin *plugin, gpointer pdata);
 
-PluginCallback plugin_callbacks[] =
+static PluginCallback plugin_callbacks[] =
 {
     {"editor-notify", (GCallback)&notify_cb, TRUE,NULL},
     {"project-open", (GCallback)&project_open_cb, TRUE,NULL},
@@ -23,6 +23,21 @@ PluginCallback plugin_callbacks[] =
     {"project-save", (GCallback)&project_save_cb, TRUE,NULL},
     {NULL,NULL,FALSE,NULL}
 };
+
+
+void geany_load_module(GeanyPlugin *plugin)
+{
+    plugin->info->name = "Lisp Development";
+    plugin->info->description = "Lisp Development";
+    plugin->info->version = "0.1";
+    plugin->info->author = "Jason Miller <jasom@fastmail.com>";
+    plugin->funcs->callbacks = plugin_callbacks;
+    plugin->funcs->init = myplugin_init;
+    plugin->funcs->cleanup = myplugin_cleanup;
+    plugin->funcs->configure = NULL;
+    plugin->funcs->help = NULL;
+    GEANY_PLUGIN_REGISTER(plugin,225);
+}
 
 static void charadded_cb(GeanyEditor *ed, SCNotification *nt)
 {
@@ -71,17 +86,25 @@ static void project_close_cb(G_GNUC_UNUSED GObject * obj,
     glispProjectClose();
 }
 
-void plugin_init(G_GNUC_UNUSED GeanyData *data)
+static gboolean myplugin_init(GeanyPlugin *plugin, G_GNUC_UNUSED gpointer pdata)
 {
-    keybindings_set_item(plugin_key_group, 0, glispKbRunComplete,
+    GeanyKeyGroup *kg = plugin_set_key_group(plugin, "lisp", 2, NULL);
+    
+    geany_data = plugin->geany_data;
+    geany_plugin = plugin;
+
+    keybindings_set_item(kg, 0, glispKbRunComplete,
             0, 0, "run_lisp_complete", "Complete lisp symbol", NULL);
-    keybindings_set_item(plugin_key_group, 1, glispKbRunJump,
+    keybindings_set_item(kg, 1, glispKbRunJump,
             0, 0, "run_lisp_jump", "Jump to Lisp definition", NULL);
+
+
     glispProjectInit();
+
+    return TRUE;
 }
 
-void plugin_cleanup(void)
+static void myplugin_cleanup(G_GNUC_UNUSED GeanyPlugin *plugin, G_GNUC_UNUSED gpointer pdata)
 {
-    //gtk_widget_destroy(main_menu_item);
+    return;
 }
-
