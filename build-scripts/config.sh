fd2097867e980f86713e2a62ac4e4c6dfef2df32
Jason Miller <aidenn0@geocities.com>
2017-08-11 17:40:54 -0700

    Decouple install script from install directory
    This allows installing to somewhere other than @INSTALL_DIR@.
    
    Should be useful for automating building of packages.

--- build-scripts/config.sh
+++ build-scripts/config.sh
@@ -1,6 +1,8 @@
 PREFIX="${PREFIX:-/usr/local}"
+FAKE_PREFIX="${FAKE_PREFIX:-$PREFIX}"
 cat > tup.config <<EOF
 CONFIG_INSTALL_DIR=${INSTALL_DIR:-$PREFIX/share/glisp}
+CONFIG_FAKE_INSTALL_DIR=${FAKE_INSTALL_DIR:-${INSTALL_DIR:-$FAKE_PREFIX/share/glisp}}
 CONFIG_GEANY_PLUGIN_DIR=${GEANY_PLUGIN_DIR:-$HOME/.config/geany/plugins}
 CONFIG_CFLAGS=${CFLAGS:--g3}
 CONFIG_LISP_CMD=${LISP_CMD:=sbcl --no-sysinit --no-userinit}
